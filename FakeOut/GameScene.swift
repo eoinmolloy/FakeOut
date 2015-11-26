7db00245099187c8f4d1d8aec8688d6fef0bca68
Eoin Molloy <eoinmolloy@outlook.com>
2015-11-26 19:23:09 +0000

    bouncing ball around the screen

--- FakeOut/GameScene.swift
+++ FakeOut/GameScene.swift
@@ -16,5 +16,14 @@ let blockNodeName = "blockNode"
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
+        
+        //you create a border around the screen, this has no volume or mass and has a friction of 0
+        let border = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        border.friction = 0
+        self.physicsBody = border
+        //Gravity is removed and a force is applied to the ball
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        let ball = childNodeWithName(ballName) as! SKSpriteNode
+        ball.physicsBody!.applyImpulse(CGVectorMake(10, -10))
     }
 }
